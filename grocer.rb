21c2ee7c7e931ce684ce2c669d5e7af7bd7f0f69
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-10 10:43:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -102,7 +102,7 @@ total = 0
 index = 0
 while index < final_cart.length do
 
-total += final_cart[index][:price] + final_cart[index][:count]
+total += final_cart[index][:price] * final_cart[index][:count]
 
 if total > 100
  total - total * 20 / 100
