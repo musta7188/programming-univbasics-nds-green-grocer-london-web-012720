25c06171bf5dde059bd4afb0e7b2640e1238a32f
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-10 11:06:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,7 +5,7 @@ def find_item_by_name_in_collection(name, collection)
 index  = 0
 while index < collection.length do
   if collection[index][:item] === name
-    return collection[index]
+   collection[index]
   end
   index += 1
 end
