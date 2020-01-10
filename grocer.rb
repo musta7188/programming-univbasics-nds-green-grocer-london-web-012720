ad74dae253ddcb5e3636849a94d4c0fa47b76079
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-10 11:04:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,11 +40,6 @@ index += 1
 end
 return new_array
 
-
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 end
 
 
