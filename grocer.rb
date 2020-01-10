5780e25820f31abf35e4d8114ee9854e61af5448
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-10 11:14:47 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,36 +1,115 @@
+require "pry"
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+
+#return the intire hash if equal to the argument name given
+index  = 0
+while index < collection.length do
+  if collection[index][:item] === name
+   return collection[index]
+ else
+   nil
+  end
+  index += 1
+end
+
+
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+
+new_array = []
+index = 0
+
+while index < cart.length do
+
+
+cart_item = find_item_by_name_in_collection(cart[index][:item], new_array)
+
+if cart_item != nil
+  cart_item[:count] += 1
+else
+  cart_item = {
+    :item => cart[index][:item],
+    :price => cart[index][:price],
+    :clearance => cart[index][:clearance],
+    :count => 1
+  }
+  new_array << cart_item
 end
 
+index += 1
+
+end
+new_array
+
+end
+
+
 def apply_coupons(cart, coupons)
+
+index = 0
+while index < coupons.length do
+
+cart_item = find_item_by_name_in_collection(coupons[index][:item], cart)
+coupons_item_name = "#{coupons[index][:item]} W/COUPON"
+cart_item_with_coupon = find_item_by_name_in_collection(coupons_item_name, cart)
+if coupons_item_name && cart_item[:count] >= coupons[index][:num]
+  if cart_item_with_coupon
+    cart_item_with_coupon[:count] += coupons[index][:num]
+  else
+    cart_item_with_coupon = {
+      item: coupons_item_name,
+      price: coupons[index][:cost] / coupons[index][:num],
+      count: coupons[index][:num],
+      clearance: cart_item[:clearance]
+
+    }
+    cart << cart_item_with_coupon
+    cart_item[:count] -= coupons[index][:num]
+
+  end
+end
+  index += 1
+end
+cart
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
 end
 
 def apply_clearance(cart)
+  index  = 0
+  while index < cart.length do
+
+    if cart[index][:clearance]
+      cart[index][:price] =  (cart[index][:price] - (cart[index][:price] * 0.2))
+  end
+  index += 1
+
+  end
+  cart
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+
+new_cart = consolidate_cart(cart)
+apply_coup =  apply_coupons(new_cart, coupons)
+final_cart = apply_clearance(apply_coup)
+
+total = 0
+index = 0
+while index < final_cart.length do
+
+total += final_cart[index][:price] * final_cart[index][:count]
+index += 1
+end
+if total > 100
+ total -= (total * 0.10)
+
+end
+return total
+
 end
